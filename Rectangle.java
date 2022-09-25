fdd31579634417b09c10d5cfe2e7cdb6c51314eb
unknown <ibraheemhaseeb7@gmail.com>
2022-09-25 20:02:05 +0500

    added area function

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,7 @@
 class Rectangle {
 
   // attributes
-  public int x1, y1, height, width, x2, y2, x3, y3, x4, y4;
+  public int x1, y1, height, width, x2, y2, x3, y3, x4, y4, area;
 
   // constructor
   public Rectangle(int height, int width, int x1, int y1) {
@@ -10,12 +10,14 @@ class Rectangle {
     this.x1 = x1;
     this.y1 = y1;
     CalculateParameters();
+    this.area = area();
   }
 
   // returns a string that has all the attributes
   String showAttributes() {
-    return String.format("Height: %d\nWidth: %d\nX1: %d\tY1: %d\nX2: %d\tY2: %d\nX3: %d\tY3: %d\nX4: %d\tY4: %d",
-        height, width, x1, y1, x2, y2, x3, y3, x4, y4);
+    return String.format(
+        "Height: %d\nWidth: %d\nArea: %d\nX1: %d\tY1: %d\nX2: %d\tY2: %d\nX3: %d\tY3: %d\nX4: %d\tY4: %d",
+        height, width, area, x1, y1, x2, y2, x3, y3, x4, y4);
   }
 
   // calculates attributes of the rectangle on the basis of width, height, x1 and
@@ -27,5 +29,9 @@ class Rectangle {
     y3 = y1 + height;
     x4 = x1 + width;
     y4 = y1 + height;
+  }
+
+  public int area() {
+    return height * width;
   }
 }
